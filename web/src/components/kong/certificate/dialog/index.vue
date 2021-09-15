b0165db4acdb2de09fc2871b58508bcb437062ff
geange <geange@outlook.com>
2021-09-15 20:25:26 +0800

    feat: 1. rm .idea 2. add service available

---
+++ web/src/components/kong/certificate/dialog/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <el-dialog :before-close="closeDialog" :visible.sync="visible" title="客户">
+      <el-form :inline="true" :model="form" label-width="80px">
+        <el-form-item label="客户名">
+          <el-input v-model="form.customerName" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="客户电话">
+          <el-input v-model="form.customerPhoneData" autocomplete="off" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="closeDialog">Cancel</el-button>
+        <el-button type="primary" @click="enterDialog">Create</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    visible: {
+      require: true,
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      form: {
+        customerName: '',
+        customerPhoneData: ''
+      }
+    }
+  },
+  methods: {
+    closeDialog() {
+      this.$emit('closeHandler')
+    },
+    enterDialog() {
+      console.log('enter')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- web/src/components/kong/route/table/index.vue
+++ web/src/components/kong/route/table/index.vue
@@ -1,35 +1,53 @@
 <template>
-  <el-table :data="routes" style="width: 100%">
-    <el-table-column label="ID">
-      <template slot-scope="item">
-        <el-button type="text">{{ item.row.id }}</el-button>
-      </template>
-    </el-table-column>
-    <el-table-column label="Name" prop="name" />
-    <el-table-column label="Paths" prop="paths">
-      <template slot-scope="props">
-        <el-tag v-for="path in props.row.paths" :key="path" size="mini">{{ path }}</el-tag>
-      </template>
-    </el-table-column>
-    <el-table-column label="Tags" prop="tags">
-      <template slot-scope="props">
-        <el-tag v-for="tag in props.row.tags" :key="tag" size="mini">{{ tag }}</el-tag>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div>
+    <div class="button-box clearflex">
+      <el-button size="mini" type="primary" icon="el-icon-plus" @click="add">ADD</el-button>
+    </div>
+    <el-table :data="routes" style="width: 100%">
+      <el-table-column label="ID">
+        <template slot-scope="item">
+          <el-button type="text">{{ item.row.id }}</el-button>
+        </template>
+      </el-table-column>
+      <el-table-column label="Name" prop="name" />
+      <el-table-column label="Paths" prop="paths">
+        <template slot-scope="props">
+          <el-tag v-for="path in props.row.paths" :key="path" size="mini">{{ path }}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column label="Tags" prop="tags">
+        <template slot-scope="props">
+          <el-tag v-for="tag in props.row.tags" :key="tag" size="mini">{{ tag }}</el-tag>
+        </template>
+      </el-table-column>
+    </el-table>
+    <Dialog :visible="dialogFormVisible" @closeHandler="closeDialog" />
+  </div>
 </template>
 
 <script>
+import {
+  listAllRouteInService
+} from '@/api/kongRoute'
+
+import Dialog from '@/components/kong/route/dialog'
+
 export default {
   name: 'RouteTable',
+  components: { Dialog },
   props: {
-    serviceId: {
-      type: Array,
-      default: null
+    sid: {
+      type: String,
+      default: ''
+    },
+    rtype: {
+      type: String,
+      default: ''
     }
   },
   data() {
     return {
+      dialogFormVisible: false,
       routes: [
         {
           created_at: 0,
@@ -61,44 +79,39 @@ export default {
           request_buffering: false,
           response_buffering: false
         }
-      ],
-      tableData: [{
-        id: '12987122',
-        name: '好滋好味鸡蛋仔',
-        category: '江浙小吃、小吃零食',
-        desc: '荷兰优质淡奶，奶香浓而不腻',
-        address: '上海市普陀区真北路',
-        shop: '王小虎夫妻店',
-        shopId: '10333'
-      }, {
-        id: '12987123',
-        name: '好滋好味鸡蛋仔',
-        category: '江浙小吃、小吃零食',
-        desc: '荷兰优质淡奶，奶香浓而不腻',
-        address: '上海市普陀区真北路',
-        shop: '王小虎夫妻店',
-        shopId: '10333'
-      }, {
-        id: '12987125',
-        name: '好滋好味鸡蛋仔',
-        category: '江浙小吃、小吃零食',
-        desc: '荷兰优质淡奶，奶香浓而不腻',
-        address: '上海市普陀区真北路',
-        shop: '王小虎夫妻店',
-        shopId: '10333'
-      }, {
-        id: '12987126',
-        name: '好滋好味鸡蛋仔',
-        category: '江浙小吃、小吃零食',
-        desc: '荷兰优质淡奶，奶香浓而不腻',
-        address: '上海市普陀区真北路',
-        shop: '王小虎夫妻店',
-        shopId: '10333'
-      }]
+      ]
     }
   },
   created() {
-    console.log('create')
+  },
+  mounted() {
+    console.log('create routes', this.sid, this.rtype)
+    console.log('======>', this.rtype)
+    if (this.rtype === 'service') {
+      this.getRoutesByService(this.sid)
+    }
+  },
+  methods: {
+    add() {
+      console.log('add new route')
+      this.dialogFormVisible = true
+    },
+    closeDialog() {
+      this.dialogFormVisible = false
+    },
+    async getRoutesByService(id) {
+      console.log('get route')
+      const params = { id: id }
+      listAllRouteInService(params).then((rs) => {
+        console.log(rs)
+        if (rs.msg === 'listRouteError') {
+          this.routes = []
+        }
+      }).catch(err => {
+        this.$message({ type: 'error', message: err })
+        this.routes = []
+      })
+    }
   }
 }
 </script>
