712b38e21c0d25425037557fbcd09b643006ebde
geange <geange@outlook.com>
2021-09-23 23:26:02 +0800

    feat: kong service edit

--- web/src/components/kong/service/editor/index.vue
+++ web/src/components/kong/service/editor/index.vue
@@ -4,28 +4,40 @@
       <el-tag>{{ service.id }}</el-tag>
     </el-form-item>
     <el-form-item label="Name">
-      <el-input v-model="service.name" style="width: 300px" />
-    </el-form-item>
-    <el-form-item label="Path">
-      <el-input v-model="service.path" style="width: 300px" />
-    </el-form-item>
-    <el-form-item label="Port">
-      <el-input v-model="service.port" style="width: 300px" type="number" />
+      <el-input v-model="service.name" />
+      <p>The Service name.</p>
     </el-form-item>
     <el-form-item label="Protocol">
-      <el-input v-model="service.protocol" style="width: 300px" />
+      <el-input v-model="service.protocol" />
+      <p>The protocol used to communicate with the upstream. Accepted values are: <font>"grpc", "grpcs", "http", "https", "tcp", "tls", "udp"</font>. Default: <font>"http"</font>.</p>
     </el-form-item>
-    <el-form-item label="ReadTimeout">
-      <el-input v-model="service.read_timeout" style="width: 300px" type="number" />
+    <el-form-item label="Host">
+      <el-input v-model="service.host" />
+      <p>The host of the upstream server.</p>
     </el-form-item>
-    <el-form-item label="Retries">
-      <el-input v-model="service.retries" style="width: 300px" type="number" />
+    <el-form-item label="Port">
+      <el-input v-model="service.port" type="number" />
+      <p>The upstream server port. Default: <font>80</font>.</p>
     </el-form-item>
-    <el-form-item label="URL">
-      <el-input v-model="service.url" style="width: 300px" />
+    <el-form-item label="Path">
+      <el-input v-model="service.path" />
+      <p>The path to be used in requests to the upstream server.</p>
+    </el-form-item>
+    <el-form-item label="Connect Time">
+      <el-input v-model="service.connect_timeout" type="number" />
+      <p>The timeout in milliseconds for establishing a connection to the upstream server. Default: <font>60000</font>.</p>
     </el-form-item>
     <el-form-item label="WriteTimeout">
-      <el-input v-model="service.write_timeout" style="width: 300px" type="number" />
+      <el-input v-model="service.write_timeout" type="number" />
+      <p>The timeout in milliseconds between two successive write operations for transmitting a request to the upstream server. Default: <font>60000</font>.</p>
+    </el-form-item>
+    <el-form-item label="ReadTimeout">
+      <el-input v-model="service.read_timeout" type="number" />
+      <p>The timeout in milliseconds between two successive read operations for transmitting a request to the upstream server. Default: <font>60000</font>.</p>
+    </el-form-item>
+    <el-form-item label="Retries">
+      <el-input v-model.number="service.retries" type="number" />
+      <p>The number of retries to execute upon failure to proxy. Default: <font>5</font>.</p>
     </el-form-item>
     <el-form-item label="Tags">
       <el-tag v-for="tag in service.tags" :key="tag" closable :disable-transitions="false" @close="handleTagsClose(tag)">
@@ -33,12 +45,19 @@
       </el-tag>
       <el-input v-if="inputTagsVisible" ref="saveTagsInput" v-model="inputTagValue" class="input-new-tag" size="small" @keyup.enter.native="handleTagsInputConfirm" @blur="handleTagsInputConfirm" />
       <el-button v-else class="button-new-tag" size="small" @click="showTagsInput">+ New Tag</el-button>
+      <p>An optional set of strings associated with the Service for grouping and filtering.</p>
+    </el-form-item>
+    <el-form-item label="Client Certificate">
+      <el-input v-model="service.client_certificate.id" />
+      <p>Certificate to be used as client certificate while TLS handshaking to the upstream server. With form-encoded, the notation is <font>client_certificate.id=${client_certificate id}</font>. With JSON, use <font>"client_certificate":{"id":"${client_certificate id}"}</font>.</p>
     </el-form-item>
     <el-form-item label="TLSVerify">
       <el-switch v-model="service.tls_verify" />
+      <p>Whether to enable verification of upstream server TLS certificate. If set to <font>null</font>, then the Nginx default is respected.</p>
     </el-form-item>
-    <el-form-item label="TLSVerifyDepth">
-      <el-input v-model="service.tls_verify_depth" style="width: 300px" type="number" />
+    <el-form-item label="TLS Verify Depth">
+      <el-input v-model="service.tls_verify_depth" type="number" />
+      <p>Maximum depth of chain while verifying Upstream server’s TLS certificate. If set to <font>null</font>, then the Nginx default is respected. Default: <font>null</font>.</p>
     </el-form-item>
     <el-form-item label="CACertificates">
       <el-tag v-for="cert in service.ca_certificates" :key="cert" closable :disable-transitions="false" @close="handleCertsClose(cert)">
@@ -46,6 +65,11 @@
       </el-tag>
       <el-input v-if="inputCertsVisible" ref="saveCertsTagInput" v-model="inputCertsValue" class="input-new-tag" size="small" @keyup.enter.native="handleCertsInputConfirm" @blur="handleCertsInputConfirm" />
       <el-button v-else class="button-new-tag" size="small" @click="showCertsInput">+ New Tag</el-button>
+      <p>Array of CA Certificate object UUIDs that are used to build the trust store while verifying upstream server’s TLS certificate. If set to null when Nginx default is respected. If default CA list in Nginx are not specified and TLS verification is enabled, then handshake with upstream server will always fail (because no CA are trusted). With form-encoded, the notation is ca_certificates[]=4e3ad2e4-0bc4-4638-8e34-c84a417ba39b&ca_certificates[]=51e77dc2-8f3e-4afa-9d0e-0e3bbbcfd515. With JSON, use an Array.</p>
+    </el-form-item>
+    <el-form-item label="URL">
+      <el-input v-model="service.url" />
+      <p>Shorthand attribute to set <font>protocol</font>, <font>host</font>, <font>port</font> and <font>path</font> at once. This attribute is write-only (the Admin API never returns the URL).</p>
     </el-form-item>
     <el-form-item>
       <el-button type="primary" @click="onSubmit">Update</el-button>
@@ -175,6 +199,7 @@ export default {
         host: this.service.host,
         name: this.service.name
       }
+      console.log(this.service.retries)
       this.service.client_certificate.id !== '' && (data.client_certificate.id = this.service.client_certificate.id)
       this.service.path !== '' && (data.path = this.service.path)
       this.service.port !== 0 && (data.port = this.service.port)
@@ -187,6 +212,7 @@ export default {
       this.service.tls_verify !== false && (data.tls_verify = this.service.tls_verify)
       this.service.tls_verify_depth !== 0 && (data.tls_verify_depth = this.service.tls_verify_depth)
       this.service.ca_certificates !== [] && (data.ca_certificates = this.service.ca_certificates)
+      console.log(data)
       updateService(data).then((rs) => {
       }).catch(err => {
         this.$message({ type: 'error', message: err })
@@ -225,5 +251,17 @@ export default {
     width: 120px;
     margin-left: 10px;
     vertical-align: bottom;
+  }
+  p {
+    font-size: 13px;
+    color: #8b95aa;
+    line-height: 1.5;
+    line-break: strict;
+    margin-right: 20px;
+  }
+  font {
+    font-size: 13px;
+    font-weight: bold;
+    color: #fa6863;
   }
 </style>
